3ef0bcd194d826cf086c268870fae78951f46002
lulei1020 <lulei1020@126.com>
2015-10-23 18:14:22 +0800

    Add：Login TestCases

---
+++ TestCase/Login/Web_Login_by_mail.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from Public import P_Login_out_web
+from Page import PageImp
+import time
+
+
+def web_login_by_mail():
+
+    P_Login_out_web.Login_and_out.Login()
+    P_Login_out_web.Login_and_out.Logout()
+

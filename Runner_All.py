ca5c0fd31abb5b759ec9c211be06f34170725188
lulei1020 <lulei1020@126.com>
2015-11-03 11:34:07 +0800

    做了一些优化

--- Runner_All.py
+++ Runner_All.py
@@ -2,6 +2,7 @@
 
 from Automan import PublicImp
 import os
+from time import sleep
 
 
 # PC/APP/H5
@@ -10,15 +11,27 @@ def run_all_case():
 
     for filename in os.listdir("TestCase"):
         filepath = os.getcwd() + "\\TestCase\\" + filename
+
+        currentpath = os.getcwdu()
+        source_name = PublicImp.common.get_value_from_conf_path("TESTING_BROWSERS_OR_DEVICES", currentpath)
+        if source_name.split("|")[0] in ("Chrome", "Firefox", "IE", "Safari"):
+            source = "Web"
+        elif source_name == "APP-Android":
+            source = "Android"
+        elif source_name == "APP-IOS":
+            source = "IOS"
+        elif source_name == "H5-Android" or source_name == "H5-iOS":
+            source = "H5"
+
         if os.path.isdir(filepath):
             for filename2 in os.listdir(filepath):
-                if filename2.split("_")[0] == "Web" and os.path.splitext(filename2)[1] == '.py':
+                if filename2.split("_")[0] == source and os.path.splitext(filename2)[1] == '.py':
                     name = filename2.split(".")[0]
                     print("=> **********************************")
                     print("=> Soon Will run the test case ** %s->%s->%s.py **" % ("TestCase", filename, name))
                     PublicImp.executer.run_module(filename, name)
         else:
-            if filename.split("_")[0] == "Web" and os.path.splitext(filename)[1] == '.py':
+            if filename.split("_")[0] == source and os.path.splitext(filename)[1] == '.py':
                 name = filename.split(".")[0]
                 print("=> **********************************")
                 print("=> Soon Will run the test case ** %s->%s.py **" % ("TestCase", name))
@@ -30,6 +43,10 @@ def run_all_case():
 # 执行开始时间
 Executer_Date = PublicImp.common.stamp_date()
 StartTime = PublicImp.common.stamp_date_nomal()
+
+# 启动Appium Server服务
+# PublicImp.AppiumServer.AppiumServer.startServer()
+# sleep(5)
 
 # 创建PageObject文件
 print("=> Generating PageObjects py file from mysql ...")
@@ -48,3 +65,6 @@ PublicImp.env.ExecuterDate = Executer_Date
 
 # 发送测试报告邮件
 PublicImp.sendreport.sendreport()
+
+# 关闭Appium Server服务
+# PublicImp.AppiumServer.AppiumServer.stopServer()

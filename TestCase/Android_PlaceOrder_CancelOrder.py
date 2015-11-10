62985a9dc82f8ede1ddca36d7ee1c275e81ebec2
lulei1020 <lulei1020@126.com>
2015-11-10 17:53:07 +0800

    增加了取消订单测试用例，用于所有下单用例执行完后检查是否有未取消的订单，有则进行取消；

---
+++ TestCase/Android_PlaceOrder_CancelOrder.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from Public import P_Login_out_android
+from Page import PageImp
+from Automan import PublicImp
+from appium.webdriver.connectiontype import ConnectionType
+from time import sleep
+import datetime
+
+
+def testcase_Android_PlaceOrder():
+
+    P_Login_out_android.Login_And_out.Login()
+
+    PageImp.Page_Home.Page_Home.tabMain_my.Click()
+
+    u""" 个人中心-我的订单-待付款-取消订单 """
+    # PageImp.Page_PersonCenter.Page_PersonCenter.order_mine_dfk.Click()
+    # PageImp.Page_PersonCenter.Page_PersonCenter.order_item_cancel_btn.ClickList_App()
+    # PageImp.Page_PersonCenter.Page_PersonCenter.order_dialog_confirm_btn.Click()
+    # PageImp.Page_PersonCenter.Page_PersonCenter.order_back_imgbtn.Click()
+
+    PageImp.Page_PersonCenter.Page_PersonCenter.order_mine_dfk.Click()
+
+    if PageImp.Page_PersonCenter.Page_PersonCenter.order_item_cancel_btn.IsExist():
+        cancelcount = PageImp.Page_PersonCenter.Page_PersonCenter.order_item_cancel_btn.GetObjectsCount()
+        i = cancelcount
+        while i > 0:
+            PageImp.Page_PersonCenter.Page_PersonCenter.order_item_cancel_btn.ClickList_App()
+            PageImp.Page_PersonCenter.Page_PersonCenter.order_dialog_confirm_btn.Click()
+            i = PageImp.Page_PersonCenter.Page_PersonCenter.order_item_cancel_btn.GetObjectsCount()
+
+    PageImp.Page_PersonCenter.Page_PersonCenter.order_back_imgbtn.Click()
+
+    u""" 退出登录 """
+    P_Login_out_android.Login_And_out.Logout()

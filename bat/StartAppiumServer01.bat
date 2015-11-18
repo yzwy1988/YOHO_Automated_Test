231ecf4118e65fa897f2e807edeacea17374b63f
lulei1020 <lulei1020@126.com>
2015-11-18 14:25:24 +0800

    删除了一些文件

--- bat/StartAppiumServer01.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-echo Start Appium Server 01 ...
-appium -a 172.16.6.211 -p 4723 -bp 4724
--- bat/StartAppiumServer02.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off  
-echo Start Appium Server 02 ...
-appium -a 172.16.6.211 -p 4725 -bp 4726

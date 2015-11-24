088a98ee54ee6c79faae583c6f69eb4b8390ab2b
lulei1020 <lulei1020@126.com>
2015-11-24 10:37:07 +0800

    增加了启动、停止AppiumServer函数

---
+++ Automan/AppiumServer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+import common
+
+
+def startServer():
+    currentpath = os.getcwdu()
+    source_name = common.get_value_from_conf_path("TESTING_BROWSERS_OR_DEVICES", currentpath)
+    if source_name in ("APP-Android", "APP-IOS", "H5-Android", "H5-iOS"):
+        os.popen("taskkill /f /im cmd.exe")
+        os.popen("taskkill /f /im node.exe")
+        os.system(os.getcwdu() + '\Bat\StartAppiumServer.Bat')
+
+
+def endServer():
+    os.popen("taskkill /f /im cmd.exe")
+    os.popen("taskkill /f /im node.exe")
+
+
+if __name__ == "__main__":
+    startServer()

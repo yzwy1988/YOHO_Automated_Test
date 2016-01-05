025a39242a75f6a35ae6e2671ed7a86a0721db82
lulei1020 <lulei1020@126.com>
2016-01-05 16:39:11 +0800

    做了优化

--- Runner_All.py
+++ Runner_All.py
@@ -2,6 +2,7 @@
 
 from Automan import PublicImp
 import os
+import time
 
 
 # PC/APP/H5
@@ -16,7 +17,7 @@ def run_all_case():
         source = "Android"
     elif source_name == "APP-IOS":
         source = "IOS"
-    elif source_name == "H5-Android" or source_name == "H5-iOS":
+    elif source_name == "H5-Android" or source_name == "H5-IOS":
         source = "H5"
 
     for filename in os.listdir("TestCase"):
@@ -28,23 +29,28 @@ def run_all_case():
                     print("=> **********************************")
                     print("=> Soon Will run the test case ** %s->%s->%s.py **" % ("TestCase", filename, name))
                     PublicImp.executer.run_module(filename, name)
+                    # time.sleep(70)
         else:
             if filename.split("_")[0] == source and os.path.splitext(filename)[1] == '.py':
                 name = filename.split(".")[0]
                 print("=> **********************************")
                 print("=> Soon Will run the test case ** %s->%s.py **" % ("TestCase", name))
                 PublicImp.executer.run_module("", name)
+                # time.sleep(70)
 
     # 生成\\result\\result.xls文件
     PublicImp.log.generate_result_xls()
 
-# 执行开始时间
-Executer_Date = PublicImp.common.stamp_date()
-StartTime = PublicImp.common.stamp_date_nomal()
+# 开启AppiumServer
+PublicImp.AppiumServer.startServer()
 
 # 创建PageObject文件
 print("=> Generating PageObjects py file from mysql ...")
 PublicImp.CreatePageFromDB.create_page_from_db()
+
+# 执行开始时间
+Executer_Date = PublicImp.common.stamp_date()
+StartTime = PublicImp.common.stamp_date_nomal()
 
 # 运行testcase
 run_all_case()
@@ -59,3 +65,6 @@ PublicImp.env.ExecuterDate = Executer_Date
 
 # 发送测试报告邮件
 PublicImp.sendreport.sendreport()
+
+# 停止AppiumServer
+PublicImp.AppiumServer.endServer()

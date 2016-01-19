188ad722666fa5481d5b3caad5ed7d4503c6fbd1
lulei1020 <lulei1020@126.com>
2016-01-19 21:42:13 +0800

    增加测试用例：创意生活首页测试用例；

---
+++ TestCase/07_Home/Web_CommodityFromLS.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from Page import PageImp
+from Public import P_Public_Imp
+from Automan import PublicImp
+from time import sleep
+
+"""
+   测试用例描述：LifeStyle_人气单品、最新上架图片链接跳转, 选择商品加入购物车;
+"""
+
+
+def testcase_Web_CommodityFromLS():
+
+    PageImp.Page_HomeGuide.Page_HomeGuide.GoLifeStyle.Click()
+
+    PageImp.Page_Home.Page_Home.CommodityGoods.ClickList()
+    sleep(5)
+
+    # 商品详情页面选择有库存商品加入购物车;
+    P_Public_Imp.P_Web_GoodsDetails.P_Web_GoodsDetails.Web_GoodsDetails()
+
+    # 购物车-选择赠品
+    if PageImp.Page_ShopCart.Page_ShopCart.Mark_Zeng.IsExist():
+        PageImp.Page_ShopCart.Page_ShopCart.Select_Mark_Zeng.Click()
+        PageImp.Page_ShopCart.Page_ShopCart.Mark_Zeng_Select_Color.ClickList()
+        PageImp.Page_ShopCart.Page_ShopCart.Mark_Zeng_Select_Size.ClickList()
+        PageImp.Page_ShopCart.Page_ShopCart.Mark_Zeng_AddToCart.Click()
+        sleep(5)
+
+    # 购物车列表页面点击去结算按钮
+    PageImp.Page_ShopCart.Page_ShopCart.ToSettleAccounts.Click()
+    sleep(5)
+
+
